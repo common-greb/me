414b6409dd4bba130fff84cdd2561e66a3f09273
Daniel Parker <dan.stephen.parker@gmail.com>
2023-07-03 15:33:50 +1000

    set3 exercise 3 DONE

--- set3/exercise3.py
+++ set3/exercise3.py
@@ -5,13 +5,50 @@ Steps on the way to making your own guessing game.
 
 import random
 
+def get_valid_integer(prompt):
+  while True:
+    try:
+      value = int(input(prompt))
+      return value
+    except ValueError:
+      print("please enter a valid integer")
+
 
 def advancedGuessingGame():
+    print("\nWelcome to the guessing game!")
+    print("A number between _ and _ ?")
+    
+    lowerBound = get_valid_integer("Enter a lower bound: ")
+    upperBound = get_valid_integer("Enter an upper bound: ")
+    
+    print(f"OK then, a number between {lowerBound} and {upperBound} ?")
+
+    actualNumber = random.randint(lowerBound, upperBound)
+
+    guessed = False
+
+    while not guessed:
+      try:
+        guessedNumber = int(input("Guess a number: "))
+        print(f"You guessed {guessedNumber},")
+        if guessedNumber == actualNumber:
+            print(f"You got it!! It was {actualNumber}")
+            guessed = True
+        elif guessedNumber < lowerBound or guessedNumber > upperBound:
+            print("Not even in the ballpark, silly!")
+        elif guessedNumber < actualNumber:
+            print("Too small, try again :'(")
+        else:
+            print("Too big, try again :'(")
+      except ValueError:
+        print("Please enter a valid integer")
+    return "You got it!"
+
     """Play a guessing game with a user.
 
     The exercise here is to rewrite the exampleGuessingGame() function
     from exercise 3, but to allow for:
-    * a lower bound to be entered, e.g. guess numbers between 10 and 20
+    * a lower bound to be entered, e.g. guess numbers between 10 and 20 
     * ask for a better input if the user gives a non integer value anywhere.
       I.e. throw away inputs like "ten" or "8!" but instead of crashing
       ask for another value.
@@ -31,6 +68,7 @@ def advancedGuessingGame():
 
     return "You got it!"
     # the tests are looking for the exact string "You got it!". Don't modify that!
+
 
 
 if __name__ == "__main__":
